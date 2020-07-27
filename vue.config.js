d5216ffef2fd5feec223cdae5b039ff43d171e8b
BLADE <1012582116@qq.com>
2020-07-27 18:32:05 +0800

    vueconfig配置代理转发

--- vue.config.js
+++ vue.config.js
@@ -5,10 +5,29 @@ function resolve(dir) {
 }
 
 module.exports = {
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     config.resolve.alias
-    .set('@', resolve('src')) // 设置路径别名
-    .set('@pages', resolve('src/views/pages')) // 设置路径别名
+      .set('@', resolve('src')) // 设置路径别名
+      .set('@pages', resolve('src/views/pages')) // 设置路径别名
   },
-  publicPath: process.env.VUE_APP_BASE_PATH || '/'
+  devServer: {
+    proxy: (() => {
+      if (!process.env.VUE_APP_BACK_END_URL) {
+        return false
+      } else {
+        // 代理转发
+        const _basePath = process.process.env.VUE_APP_BACK_END_URL.endsWith('/') ? process.env.VUE_APP_BASE_PATH : process.env.VUE_APP_BASE_PATH + '/'
+        const _path = _basePath + 'api/'
+        const result = {}
+        result[_path] = {
+          target: process.env.VUE_APP_BACK_END_URL,
+          changeOrigin: true,
+          pathRewrite: {},
+        }
+        result[_path].pathRewrite['^' + _path] = '/'
+        return result
+      }
+    })(),
+  },
+  publicPath: process.env.VUE_APP_BASE_PATH || '/',
 }
